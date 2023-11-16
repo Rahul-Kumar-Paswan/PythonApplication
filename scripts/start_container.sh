38ef21ca4ac2712b6b2422ca730f34aad7101e8d
Rahul-Kumar-Paswan <123447842+Rahul-Kumar-Paswan@users.noreply.github.com>
2023-11-16 12:48:45 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,10 +2,9 @@
 set -e
 
 # Authenticate Docker with ECR
-/usr/bin/aws ecr get-login-password --region us-east-1 | /usr/bin/docker login --username AWS --password-stdin 571888835380.dkr.ecr.us-east-1.amazonaws.com
-
+/usr/bin/aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/i4l4g3c7
 # Pull the Docker image from ECR
-/usr/bin/docker pull 571888835380.dkr.ecr.us-east-1.amazonaws.com/python-repo:latest
+/usr/bin/docker pull  rahul/repo:latest public.ecr.aws/i4l4g3c7/rahul/repo:latest
 
 # Run the Docker image as a containers
-/usr/bin/docker run -d -p 8000:5000 571888835380.dkr.ecr.us-east-1.amazonaws.com/python-repo:latest
+/usr/bin/docker run -d -p 8000:5000 rahul/repo:latest public.ecr.aws/i4l4g3c7/rahul/repo:latest
